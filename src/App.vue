00ec95911bfaf3081d0d8cd4d39b6100541fd626
al.avery <al.avery.dev@gmail.com>
2019-06-12 15:10:36 -0500

    make addwords component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AddWords msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import AddWords from './components/AddWords.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    AddWords,
   }
 }
 </script>
---
+++ src/components/AddWords.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>Add Words</div>
+</template>
+
+<script>
+export default {
+    name: 'AddWords',
+}
+</script>
